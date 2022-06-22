6c26d5c73c48e14330136690fc161cd6463e4d9c
daravhel <lea.mg90@gmail.com>
2022-06-21 21:36:46 -0300

    primer commit

---
+++ index.js
@@ -0,0 +1,96 @@
+// if (false) {
+//     console.log("hola")
+// } else {
+//     console.log("chau")
+// }
+
+
+// let nombre = "andres";
+
+// if (nombre === "camila") {
+//     console.log("hola camila");
+// } else {
+//     console.log("hola andres");
+// }
+
+
+
+
+
+// let edad = 15;
+
+// if(edad < 15) { console.log("necesitas un adulto")}
+// else if(edad < 18) {console.log("entras con un amigo")}
+// else {console.log("podes entrar solo")}
+
+
+
+
+
+// variables booleanas
+
+// let permiso = edad > 18;
+
+// if(permiso){
+//     console.log("puedes entrar")
+// } else{
+//     console.log("no puedes entrar")
+// }
+
+
+// if(""){
+//     console.log("puedes entrar")
+// } else{
+//     console.log("no puedes entrar")
+// }
+
+
+// if(0){
+//     console.log("puedes entrar")
+// } else{
+//     console.log("no puedes entrar")
+// }
+
+
+// let nombre = "Juan carlos";
+// if (" ") {
+//     console.log("hola ", nombre, ", que haces chupa pija.")
+// } else {
+//     console.log("falta nombre")
+// }
+
+
+
+// if(nombre === "matias" || nombre === "matías") {
+//     alert("hola mati");
+// }else{alert("quien sos y que haces aca??")}
+
+
+// if(nombre === "matias" && edad === "32") {
+//     alert("hola mati");
+// }else { alert("que haces aca")}
+
+
+
+// let nombre = prompt("hola, quien eres?");
+// let edad = prompt("ingresa tu edad");
+// let concatenacion = nombre +" "+ edad + " " + "años";
+
+// let numero1 = prompt("ingresa NUMERO 1");
+// let numero2 = prompt("ingresa NUMERO 2");
+// let suma = parseInt(numero1) + parseInt(numero2);
+
+// if(nombre === "matias"){
+//     alert("hola" +" "+ concatenacion);
+// } else {
+//     alert(suma);
+// }
+
+
+let nombre = prompt("Quien sos?")
+
+if (true) {
+    alert(" Hola " + nombre + ", que haces chupa pija?");
+} else {
+    alert("pone tu nombre, corneta")
+}
